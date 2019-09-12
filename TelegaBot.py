4d5809e2a99d34c93fdcdd1b08c6100278cbdc6b
Kiriliuk Oleg <kvin_kinamoto@ukr.net>
2019-09-12 17:12:00 +0300

    debugging2

--- TelegaBot.py
+++ TelegaBot.py
@@ -17,20 +17,20 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS `chats_ids` (`id` int NOT NULL AUTO INCREMENT,`chat_id` int NOT NULL UNIQUE,PRIMARY KEY (`id`));")
+    "CREATE TABLE IF NOT EXISTS chats_ids (id int NOT NULL AUTO INCREMENT,chat_id int NOT NULL UNIQUE,PRIMARY KEY (id));")
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     logger.warning("START!")
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-    cur.execute("INSERT INTO `chats_ids` VALUES(%s)", message.chat.id)
+    cur.execute("INSERT INTO chats_ids VALUES(%s)", message.chat.id)
 
 
 @bot.message_handler(commands=['alertall'])
 def allertall(message):
     logger.warning("Allerting everyone")
-    cur.execute("SELECT `chat_id` FROM `chats_ids`")
+    cur.execute("SELECT chat_id FROM chats_ids")
     chats_ids = cur.fetchall()
     for chat_id in chats_ids:
         bot.send_message(chat_id, "Произошёл кринж у пользователя - " + message.from_user.first_name)
@@ -39,7 +39,7 @@ def allertall(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     logger.warning("ANSWER!")
-    cur.execute("INSERT INTO `chats_ids` VALUES(%s)", message.chat.id)
+    cur.execute("INSERT INTO chats_ids VALUES(%s)", message.chat.id)
     bot.send_message(message.chat.id, "Здрасьте!Йопт!")
 
 
