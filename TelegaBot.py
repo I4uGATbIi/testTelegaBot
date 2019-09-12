ee45cafaa46aae6a2d957ef72db4beb16671968f
Kiriliuk Oleg <kvin_kinamoto@ukr.net>
2019-09-12 16:16:45 +0300

    add database

--- TelegaBot.py
+++ TelegaBot.py
@@ -1,8 +1,8 @@
 ﻿import telebot
 import os
 import logging
+import psycopg2
 from flask import Flask, request
-# from telegram.ext import Updater
 
 token = "477553068:AAEVHkJonLhCNMcK56v91vFcoD3SKKQUcJI"
 
@@ -10,18 +10,34 @@ bot = telebot.TeleBot(token)
 server = Flask(__name__)
 logger = logging.getLogger(__file__)
 
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS `chats_ids` (`chat_id` int NOT NULL UNIQUE,PRIMARY KEY (`chat_id`));")
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     logger.warning("START!")
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    cur.execute("INSERT INTO `chats_ids` VALUES(%s)", message.chat.id)
+
+
+@bot.message_handler(commands=['alertall'])
+def allertall(message):
+    logger.warning("Allerting everyone")
+    cur.execute("SELECT * FROM `chats_ids`")
+    chats_ids = cur.fetchall()
+    for chat_id in chats_ids:
+        bot.send_message(chat_id, "Произошёл кринж у пользователя - " + message.from_user.first_name)
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     logger.warning("ANSWER!")
-    # bot.reply_to(message, message.text)
-    bot.send_message(message.chat.id,"ТЫ ЛОХ ПА ЖЫЗНИ,ПИДАР ПО МАСТИ!")
+    cur.execute("INSERT INTO `chats_ids` VALUES(%s)", message.chat.id)
+    bot.send_message(message.chat.id, "Здрасьте!Йопт!")
 
 
 @server.route("/" + token, methods=['POST'])
@@ -33,19 +49,11 @@ def getMessage():
 
 @server.route("/")
 def webhook():
-    # PORT = int(os.environ.get('PORT', '8443'))
-    # updater = Updater(token)
-    # updater.start_webhook(listen='0.0.0.0',
-    #                       port=PORT,
-    #                       url_path=token)
     bot.remove_webhook()
-
     bot.set_webhook(url="https://calm-temple-62052.herokuapp.com/" + token)
-    # updater.idle()
     logger.warning("HOOK!")
     return "!", 200
 
 
 logger.warning("SERVERSTART!")
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000), debug=True)
-webhook()
