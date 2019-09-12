59f651c3cf8fd5210a8db1701614923faeb965f7
I4uGATbIi <i4ugatbii@gmail.com>
2019-09-12 03:41:46 +0300

    logging...

--- TelegaBot.py
+++ TelegaBot.py
@@ -1,29 +1,31 @@
 ﻿import telebot
 import os
+import logging
 from flask import Flask, request
 
 token = "477553068:AAEVHkJonLhCNMcK56v91vFcoD3SKKQUcJI"
 
 bot = telebot.TeleBot(token)
-print(bot)
 server = Flask(__name__)
+logger = logging.getLogger(__file__)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    logger.warning("START!")
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-    print("DA VIZHU YOPT")
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    print('LOVI OTVET')
+    logger.warning("ANSWER!")
     bot.reply_to(message, message.text)
 
 
 @server.route("/" + token, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    logger.warning("ANSWER!")
     return "!", 200
 
 
@@ -31,9 +33,8 @@ def getMessage():
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url="https://calm-temple-62052.herokuapp.com/" + token)
-    print("HOOK SET")
+    logger.warning("HOOK!")
     return "!", 200
 
-
-server.logger().log(1,"SUKA")
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+logger.warning("SERVERSTART!")
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000),debug=True)
