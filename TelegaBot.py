3aea6c23e0e5338c11b3521ceb99ee546a8ef858
I4uGATbIi <i4ugatbii@gmail.com>
2019-09-12 03:48:13 +0300

    modifications

--- TelegaBot.py
+++ TelegaBot.py
@@ -2,6 +2,7 @@
 import os
 import logging
 from flask import Flask, request
+from telegram.ext import Updater
 
 token = "477553068:AAEVHkJonLhCNMcK56v91vFcoD3SKKQUcJI"
 
@@ -31,10 +32,18 @@ def getMessage():
 
 @server.route("/")
 def webhook():
+    PORT = int(os.environ.get('PORT', '8443'))
+    updater = Updater(token)
+    updater.start_webhook(listen='0.0.0.0',
+                          port=PORT,
+                          url_path=token)
     bot.remove_webhook()
+
     bot.set_webhook(url="https://calm-temple-62052.herokuapp.com/" + token)
+    updater.idle()
     logger.warning("HOOK!")
     return "!", 200
 
+
 logger.warning("SERVERSTART!")
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000),debug=True)
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000), debug=True)
